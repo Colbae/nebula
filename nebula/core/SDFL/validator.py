52583234da6878e5f69ba3189dd27800f7815251
Colin Bächtold <colin.baechtold@proton.me>
2025-06-08 13:48:02 +0200

    Create node classes and ABCs for Validator, Elector and Reputator

---
+++ nebula/core/SDFL/validator.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+
+class Validator(ABC):
+    """
+    Abstract base class for validating a proposed model.
+
+    The `Validator` class is responsible for validating a model based on custom
+    criteria defined in subclasses. The validation logic should ensure that the
+    proposed model meets the required standards.
+    """
+
+    @abstractmethod
+    def validate(self, model):
+        """
+        Validates a proposed model.
+        Args:
+            model: The model to validate.
+        Returns: True if the proposed model meets the required standards.
+        """
+        pass
